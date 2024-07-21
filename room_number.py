35f5900923cb49ef295128e29d95ef815d3842bf
andrei98ok <bestik2.0@gmail.com>
2024-07-21 16:07:46 +0700

    Добавлен ввод количества этажей и подъездов от пользователя

--- room_number.py
+++ room_number.py
@@ -1,11 +1,15 @@
-def porch_and_floor(input_room):
-    porch = (input_room - 1) // 20 + 1
-    floor = (input_room - 1) % 20 // 4 + 1
+def porch_and_floor(input_floors_number, input_rooms_per_floor, input_room):
+
+    rooms_per_porch = input_floors_number * input_rooms_per_floor
+    porch = (input_room - 1) // rooms_per_porch + 1
+    floor = ((input_room - 1) % rooms_per_porch) // input_rooms_per_floor + 1
 
     return porch, floor
 
 if __name__ == "__main__":
     input_room = int(input("Введите квартиру: "))
-    porch, floor = porch_and_floor(input_room)
+    input_floors_number = int(input("Введите количество этажей: "))
+    input_rooms_per_floor = int(input("Введите количество квартир на этаже: "))
+    porch, floor = porch_and_floor(input_floors_number, input_rooms_per_floor, input_room)
     print("Подъезд: ", porch)
     print("Этаж: ", floor)
